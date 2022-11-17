482eb30724e6f61155b78951da2f6acacaf716e5
Tikhonov Ivan <ivntik77@gmail.com>
2022-11-17 22:33:50 +0600

    1

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
   $(".slider").slick({
     arrows: true,
     dots: true,
-
     slidesToShow: 3,
     slidesToScroll: 1,
 
@@ -32,13 +31,6 @@ $(document).ready(function () {
         settings: {
           slidesToShow: 2,
           slidesToScroll: 3,
-        },
-      },
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
         },
       },
     ],
